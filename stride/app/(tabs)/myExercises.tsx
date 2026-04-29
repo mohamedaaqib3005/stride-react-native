5778d099df5f062054057ea25ed1531611f1147c
Aaqib <drop.mohamedaaqib@gmail.com>
2026-04-29 10:37:41 +0530

    add startnewworkout and logout button

--- stride/app/(tabs)/myExercises.tsx
+++ stride/app/(tabs)/myExercises.tsx
@@ -27,6 +27,12 @@ function RedirectScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Workout Log Empty</Text>
+      <Pressable
+        style={styles.startNewWorkoutButton}
+        onPress={() => router.replace("/startNewWorkout")}
+      >
+        <Text style={styles.startNewWorkoutText}>Start New Workout</Text>
+      </Pressable>
       <Pressable onPress={Logout} style={styles.logoutButton}>
         <Text style={styles.logoutText}>Logout</Text>
       </Pressable>
@@ -46,15 +52,33 @@ const styles = StyleSheet.create({
     color: "#06b2cc",
     fontSize: 16,
     fontFamily: "Outfit_700Bold",
+    marginBottom: 40,
+
   },
   logoutButton: {
     backgroundColor: "red",
-    paddingVertical: 14,
-    paddingHorizontal: 32,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
   },
   logoutText: {
     color: "white",
     fontSize: 16,
-  }
+  },
+  startNewWorkoutButton: {
+    backgroundColor: "#06b2cc",
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 999,
+    marginHorizontal: 8,
+    marginBottom: 20,
+
+  },
+
+  startNewWorkoutText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "600",
+    fontFamily: "Outfit_700Bold",
+  },
 
 })
